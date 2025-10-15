c37c399458e3eeb497e93d9a61accc6da71e342c
Diego Taques <diegotaquesps3@gmail.com>
2025-10-15 18:04:11 -0400

    ConversaoExplicita

---
+++ ConveaoExplicita/ConveaoExplicita/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConveaoExplicita
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort num1 = 100;
+            byte num2 = (byte)num1;
+
+            float num3 = 2500.786f;
+            int num4 = (int)1985.75f;
+
+            char letra = (char)97;
+
+            Console.WriteLine(letra);
+            Console.ReadKey();
+        }
+    }
+}
